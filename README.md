0f0080986fcf69f73dc47a31d1658904ce39c9c7
Paul Ryley <paul@geminilabs.io>
2015-08-29 13:20:08 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Elixir JSHint
+
+## Install
+
+```bash
+$ npm install elixir-jshint --save-dev
+```
+
+## Usage
+
+### Example `gulpfile.js`
+
+```js
+var elixir = require( 'laravel-elixir' );
+
+elixir( function( mix )
+{
+    mix.jshint( ['js/**/*.js', '!js/vendor/*.js'] );
+});
+```
+
+### Advanced Example
+
+```js
+var elixir = require( 'laravel-elixir' );
+
+elixir( function( mix )
+{
+    mix.jshint( ['js/**/*.js', '!js/vendor/*.js'], {
+        lookup: true, // default is true
+        linter: 'some-jshint-module' // default is "jshint"
+    });
+});
+```
+
+# License
+
+[MIT](/LICENSE)
---
+++ index.js
@@ -0,0 +1,35 @@
+var gulp   = require( 'gulp' );
+var jshint = require( 'gulp-jshint' );
+var Elixir = require( 'laravel-elixir' );
+
+/*
+ |----------------------------------------------------------------
+ | JSHint
+ |----------------------------------------------------------------
+ |
+ | This task uses JSHint to enforce proper coding conventions and
+ | detect errors or potential problems in your JavaScript code
+ | before the files are concatenated and minified for usage.
+ */
+
+Elixir.extend( 'jshint', function( src, options )
+{
+    var paths = new Elixir.GulpPaths().src( src );
+
+    new Elixir.Task( 'jshint', function()
+    {
+        return gulp
+            .src( src )
+            .pipe( jshint( options || {} ))
+            .pipe( jshint.reporter( 'jshint-stylish' ))
+            .pipe( jshint.reporter( 'fail' ))
+            .on( 'error', function( e )
+            {
+                new Elixir.Notification().error( e, 'JSHint Failed!' )
+
+                this.emit('end');
+            })
+            .pipe( new Elixir.Notification( 'JSHint passed!' ));
+    })
+    .watch( paths.src.path );
+});
